3badf5822c2ff1036960c79b079fb3d71ccc079f
Chris Billingham <chris.billingham@gmail.com>
2019-01-04 20:46:57 +0000

    finish up blur, start writing for battle of the bands

---
+++ 01_process-audio-blur.R
@@ -0,0 +1,65 @@
+library(tidyverse)
+library(tuneR)
+library(signal)
+library(oce)
+library(caret)
+library(fs)
+library(progress)
+
+# load in the audio functions
+source("02_audio-functions.R")
+
+# get a list of all blur mp3s from the mp3 directory
+# note format in the directory is mp3/{album_name}/{track_number} - {track_name}.mp3
+# so most of this is tuned for that
+get_files <- fs::dir_ls("mp3/", recursive = TRUE, type = "file")
+
+# create the directories for the plots, this could be better
+if(!fs::dir_exists("plot/leisure")){fs::dir_create("plot/leisure")}
+if(!fs::dir_exists("plot/modern_life_is_rubbish")){fs::dir_create("plot/modern_life_is_rubbish")}
+if(!fs::dir_exists("plot/parklife")){fs::dir_create("plot/parklife")}
+if(!fs::dir_exists("plot/the_great_escape")){fs::dir_create("plot/the_great_escape")}
+if(!fs::dir_exists("plot/blur")){fs::dir_create("plot/blur")}
+if(!fs::dir_exists("plot/thirteen")){fs::dir_create("plot/thirteen")}
+if(!fs::dir_exists("plot/think_tank")){fs::dir_create("plot/think_tank")}
+
+# generate all the spectrograms and save to plot directories
+walk(get_files, chunk_and_save)
+
+# get all the new files into a big list
+# NOTE using -size as there's a problem, i've to raise a github issue for on the fs package
+get_plots <- dir_info("plot/", recursive = TRUE, type = "file") %>%
+  select(-size) %>% 
+  mutate(album = str_extract(str_replace(path, "plot/", ""), "^[a-z_]+"))
+
+# sort them into train and validate sets, create directories and then start copying files over
+set.seed(1979)
+train_index <- createDataPartition(get_plots$album, p = 0.75, list = FALSE)
+train <- get_plots[train_index, ] %>%
+  mutate(dataset = "train") %>%
+  mutate(new_path = str_replace(path, "plot/", paste0("images/", dataset, "/")))
+
+# sort out the new directory names and go and create them if needed
+train %>% 
+  transmute(new_dir = paste0("images/", dataset, "/", album)) %>% 
+  unique() %>%
+  walk(dir_create)
+
+# copy over all the files from the old to the new
+walk2(train$path, train$new_path, file_copy)
+
+# same again for the validation set
+validate <- get_plots[-train_index, ] %>%
+  mutate(dataset = "validate") %>%
+  mutate(new_path = str_replace(path, "plot/", paste0("images/", dataset, "/")))
+
+# as before create the new directories
+validate %>% 
+  transmute(new_dir = paste0("images/", dataset, "/", album)) %>% 
+  unique() %>%
+  walk(dir_create)
+
+# copy the files over
+walk2(validate$path, validate$new_path, file_copy)
+
+# right we're now ready for some DEEP LEARNING
--- 02_audio-functions.R
+++ 02_audio-functions.R
@@ -1,19 +1,20 @@
 # take in an mp3 and convert it into spectrograms covering 1 second, save the spectrogram to a directory
-
+# in here this function is to an IMAGES directory, used for initial setup
 chunk_and_save <- function(filename) {
   
   r <- readMP3(filename)
   
   album <- filename %>%
     str_replace("mp3/", "") %>%
-    str_extract("^[a-z_]+")
+    str_extract("^[a-z0-9_]+")
   
   track_name <- filename %>%
     str_extract("[A-Za-z0-9 -()_]+.mp3") %>%
     trimws() %>%
     str_replace("[0-9]{2} - ", "") %>%
     str_replace(".mp3", "") %>%
-    str_replace_all(" ", "_")
+    str_replace_all(" ", "_") %>%
+    tolower()
   
   # convert to mono from stereo, averaging both channels
   mono_r <- mono(r, "both")
@@ -22,20 +23,22 @@ chunk_and_save <- function(filename) {
   duration_total <- round(length(mono_r@left) / mono_r@samp.rate, 2)
   print(filename)
   
-  pb <- txtProgressBar(min = 1, max = floor(duration_total)-1, initial = 1, style = 3)
+  pb <- progress_bar$new(
+    format = " processing mp3 file [:bar] :percent eta: :eta",
+    clear = FALSE,
+    total = floor(duration_total)
+  )
   
   for (i in 0:(floor(duration_total)-1)) {
     chunk <- extractWave(mono_r, from = i, to = i+1, xunit = "time")
     chunk_name <- paste0("plot/", album, "/", track_name, str_pad(as.character(i), 4, side = "left", pad = "0"), ".png")
     save_spectrogram(chunk, chunk_name)
-    setTxtProgressBar(pb,i)
+    pb$tick()
   }
-  print("")
+
 }
 
-
-
-# process chunk and save off image
+# process a single chunk of music and save off image at a named location
 save_spectrogram <- function(wave_obj, filename) {
   
   # extract the left channel
@@ -72,12 +75,12 @@ save_spectrogram <- function(wave_obj, filename) {
   P <- abs(spec$S)
   
   # normalize, but only if we're not in silence
-  if(mean(P) > 0) {
+  if(!(mean(P) == 0)) {
     P <- P/max(P)
   }
   
   # convert to dB, again but not if we're in silence
-  if(mean(P) > 0) {
+  if(!(mean(P) == 0)) {
   P <- 10*log10(P)
   }
 
@@ -85,6 +88,7 @@ save_spectrogram <- function(wave_obj, filename) {
   t <- spec$t
   
   # plot spectrogram and save
+  if(!(mean(P) == 0)) {
   png(filename)
   imagep(x = t,
          y = spec$f,
@@ -95,4 +99,44 @@ save_spectrogram <- function(wave_obj, filename) {
          axes = FALSE
   )
   dump <- dev.off()
+}
+}
+
+# this is the same as chunk and save but saves into the tempdir() for individual track prediction
+chunk_and_temp <- function(filename) {
+  
+  r <- readMP3(filename)
+  
+  album <- filename %>%
+    str_replace("mp3/", "") %>%
+    str_extract("^[a-z0-9_]+")
+  
+  track_name <- filename %>%
+    str_extract("[A-Za-z0-9 -()_]+.mp3") %>%
+    trimws() %>%
+    str_replace("[0-9]{2} - ", "") %>%
+    str_replace(".mp3", "") %>%
+    str_replace_all(" ", "_") %>%
+    tolower()
+  
+  # convert to mono from stereo, averaging both channels
+  mono_r <- mono(r, "both")
+  
+  # get the duration for the entire file, we'll need this for later
+  duration_total <- round(length(mono_r@left) / mono_r@samp.rate, 2)
+  print(paste0("Converting ",basename(filename), " into ", floor(duration_total), " spectrograms"))
+  
+  pb <- progress_bar$new(
+    format = " processing mp3 file [:bar] :percent eta: :eta",
+    clear = FALSE,
+    total = floor(duration_total)
+  )
+
+  for (i in 0:(floor(duration_total)-1)) {
+    chunk <- extractWave(mono_r, from = i, to = i+1, xunit = "time")
+    chunk_name <- paste0(tempdir(), "/", track_name, str_pad(as.character(i), 4, side = "left", pad = "0"), ".png")
+    save_spectrogram(chunk, chunk_name)
+    pb$tick()
+  }
+
 }
